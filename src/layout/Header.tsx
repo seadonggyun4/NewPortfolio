3b7248d528ed2177318529e9f9eb77792b20342b
seadonggyun4 <seadonggyun@gmail.com>
2024-03-14 18:15:04 +0900

    #2, Header 컴포넌트 퍼블리싱

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,24 @@
+import module from '@/layout/header.module.css'
+import Link from "next/link";
+
+
 export default function Home() {
   return (
-    <header >
+    <header className={module.header}>
+        <h1 className={module.title}>
+          <Link href="/">Donggyun's Portfolio</Link>
+        </h1>
+        <ul className={module.menuList}>
+          <li>
+              <Link href="#profile">profile</Link>
+          </li>
+          <li>
+              <Link href="#tech-skill">tech-skill</Link>
+          </li>
+          <li>
+              <Link href="#toy-project">toy-project</Link>
+          </li>
+        </ul>
     </header>
   );
 }
